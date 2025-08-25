2a04057d57da0a82cf1ac6f161615127498a8139
channangan <channangan148@gmail.com>
2025-08-25 10:09:07 +0700

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen  grid place-content-center  bg-teal-300">
+      <Button className="p-4">App</Button>
+    </div>
+  );
+};
+
+export default App;
